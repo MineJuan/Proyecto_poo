e0101556fd47ce8cfa6617c5635322e912e4d0f0
koielpez <soparedesr@gmail.com>
2026-05-20 20:19:32 -0500

    Update EcoMision.h

--- src/EcoMision.h
+++ src/EcoMision.h
@@ -1,1 +1,21 @@
-hola
+#ifndef ECOMISION_H
+#define ECOMISION_H
+
+#include "Reserva.h"
+#include "Explorador.h"
+
+using namespace std;
+
+class EcoMision {
+private:
+    
+    Reserva* reserva;
+    Explorador* explorador;
+    
+public:
+    EcoMision();
+    void iniciar(){
+    }
+};
+
+#endif
