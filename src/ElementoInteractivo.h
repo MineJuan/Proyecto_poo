fc474383d28e29c40fa483a1674c0fcba1ba543c
koielpez <soparedesr@gmail.com>
2026-05-20 20:19:55 -0500

    Update ElementoInteractivo.h

--- src/ElementoInteractivo.h
+++ src/ElementoInteractivo.h
@@ -1,1 +1,19 @@
+#ifndef ELELMENTOINTERACTIVO_H
+#define ELELMENTOINTERACTIVO_H
 
+#include <unoerdered_map>
+#include <string>
+#include "Zona.h"
+
+using namespace std;
+
+class Explorador;
+
+class ElementosInteractivos {
+    public:
+        virtual void interactuar(Explorador* explorador = 0);
+    virtual ~ElementosInteractivo (){}
+    }
+};
+
+#endif
