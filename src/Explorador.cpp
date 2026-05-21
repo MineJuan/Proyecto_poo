13bafd9c98298747bd7bc8871138df5d139b27fb
koielpez <soparedesr@gmail.com>
2026-05-21 12:23:21 -0500

    Update Explorador.cpp

--- src/Explorador.cpp
+++ src/Explorador.cpp
@@ -1,1 +1,16 @@
+#include "Explorador.h"
+#include <iostream>
+
+using namespace std;
+
+Explorador::Explorador (string nombre){
+    this -> nombre = nombre;
+    energia = 100;
+    puntajeAmbiental = 0;
+    zonaActual = nullptr;
+};
+
+void Explorador::cambiarZona(Zona* nuevaZona){
+    zonaActual = nuevaZona;
+};
 
