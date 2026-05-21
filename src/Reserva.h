a32f7dfa8d88bfa7926fc8c997f531d9199d9e96
koielpez <soparedesr@gmail.com>
2026-05-20 20:20:45 -0500

    Update Reserva.h

--- src/Reserva.h
+++ src/Reserva.h
@@ -1,1 +1,21 @@
+#ifndef RESERVA_H
+#define RESERVA_H
 
+#include <unoerdered_map>
+#include <string>
+#include "Zona.h"
+
+using namespace std;
+
+class Reserva {
+private:
+    
+    unordered_map <string, Zona*> zonas;
+    
+public:
+    Reserva()
+    void agragarZona(strnig codigo, Zona* zona);
+    }
+};
+
+#endif
