95472e0b5268a290a01a5be067e85873560379ab
koielpez <soparedesr@gmail.com>
2026-05-20 20:20:15 -0500

    Update Explorador.h

--- src/Explorador.h
+++ src/Explorador.h
@@ -1,1 +1,24 @@
+#ifndef EXPLORADOR_H
+#define EXPLORADOR_H
+
+#include <string>
+#include "Zona.h"
+
+using namespace std;
+
+class Explorador {
+private:
+    
+    string nombre;
+    int energia;
+    int puntajeAmbiental;
+    Zona* zonaActual;
+    
+public:
+    Explorador(string nombre)
+    void cambiarZona(Zona* nuevaZona);
+    }
+};
+
+#endif
 
