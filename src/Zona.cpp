22f254776951ae3e2490ecc6bf8798cb5ba9d088
koielpez <soparedesr@gmail.com>
2026-05-21 12:24:29 -0500

    Update Zona.cpp

--- src/Zona.cpp
+++ src/Zona.cpp
@@ -1,1 +1,12 @@
+#include "zona.h"
+#include <iostream>
 
+using namespace std;
+
+Zona::Zona(string nombre) {
+  this->nombre = nombre;
+}
+
+void Zona::agregarElemento(ElementoInteractivo* elemento){
+    elementos.push_back(elemento);
+}
