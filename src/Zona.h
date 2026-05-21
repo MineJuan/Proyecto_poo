f804f42340daf4d82bca333cc1c5176ff4db97d6
koielpez <soparedesr@gmail.com>
2026-05-20 20:21:09 -0500

    Update Zona.h

--- src/Zona.h
+++ src/Zona.h
@@ -1,1 +1,22 @@
+#ifndef ZONA_H
+#define ZONA_H
 
+#include <vector>
+#include <string>
+#include "ElementoInteractivo.h"
+
+using namespace std;
+
+class Zona {
+private:
+    
+    string nombre;
+    vector <ElementoInteractivo*> elementos;
+    
+public:
+    Zona(string nombre)
+    void agragarElemento(ElememtoInteractivo* elemento);
+    }
+};
+
+#endif
