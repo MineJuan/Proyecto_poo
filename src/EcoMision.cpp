5c900b7d4772a4753a917dc706db15ddfdd561ce
koielpez <soparedesr@gmail.com>
2026-05-21 12:21:03 -0500

    Update EcoMision.cpp

--- src/EcoMision.cpp
+++ src/EcoMision.cpp
@@ -1,1 +1,15 @@
+#include "Ecomision.h"
+#include <iostream>
 
+using namespace std;
+
+EcoMision::EcoMision(){
+    reserva = new Reserva();
+    explorador = new Explorador("Juan");
+};
+
+void Ecomision::iniciar(){
+    Zona* pantano = new Zona("Pantano");
+    explorador->CambiarZona(pantano);
+    cout << "Ecomision iniciada" << endl;
+};
