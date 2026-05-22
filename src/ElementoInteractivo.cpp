20e1d2590516fdf0455658a88a8f29a965cca7e5
koielpez <soparedesr@gmail.com>
2026-05-22 11:29:43 -0500

    Update ElementoInteractivo.cpp

--- src/ElementoInteractivo.cpp
+++ src/ElementoInteractivo.cpp
@@ -1,1 +1,12 @@
+#include "ElementoInteractivo.h"
+
+ElementoInteractivo::ElementoInteractivo(string nombre) {
+
+    this->nombre = nombre;
+}
+
+string ElementoInteractivo::getNombre() {
+
+    return nombre;
+}
 
