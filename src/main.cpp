22f3ce7da616dc9fd36b5bdc89307b8fd06e75b4
koielpez <soparedesr@gmail.com>
2026-05-22 11:34:11 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,16 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    vector<int> numeros;
+
+    numeros.push_back(10);
+
+    cout << numeros[0];
+
+    return 0;
+}
 
