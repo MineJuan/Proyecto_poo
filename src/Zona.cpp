ac8ee6ba31bf265f32237e288893313d880df525
nataliaTm2 <ntorom22@gmail.com>
2026-05-22 11:32:02 -0500

    Update Zona.cpp

--- src/Zona.cpp
+++ src/Zona.cpp
@@ -7,6 +7,16 @@ Zona::Zona(string nombre) {
   this->nombre = nombre;
 }
 
+string Zona::getNombre(){
+    return nombre;
+}
+
 void Zona::agregarElemento(ElementoInteractivo* elemento){
     elementos.push_back(elemento);
+}
+
+void Zona::mostrarElementos(){
+    for (int i = 0; i < elementos.size(); i++){
+        cout << "Elemento " << i + 1 << "." << elementos[i]->getNombre() << endl;
+    }
 }
