c857083b94e9122ea6a4a2bcf16a531913689b2e
koielpez <soparedesr@gmail.com>
2026-05-22 11:28:37 -0500

    Update EcoMision.cpp

--- src/EcoMision.cpp
+++ src/EcoMision.cpp
@@ -3,13 +3,25 @@
 
 using namespace std;
 
-EcoMision::EcoMision(){
+EcoMision::EcoMision() {
     reserva = new Reserva();
     explorador = new Explorador("Juan");
+}
+
+void EcoMision::iniciar(){
+    Zona* pantano = new Zona("Pantano");
+    explorador->cambiarZona(pantano);
+    cout << "Ecomision iniciada" << endl;
+
+    mostrarRecorrido();
 };
 
-void Ecomision::iniciar(){
-    Zona* pantano = new Zona("Pantano");
-    explorador->CambiarZona(pantano);
-    cout << "Ecomision iniciada" << endl;
+void EcoMision::mostrarRecorrido(){
+    cout << "-- Iniciar Recorrido --" << endl;
+    cout << "Explorador: " << explorador->getNombre() << endl;
+    cout << "Energia: " << explorador->getEnergia() << endl;
+    cout << "Puntaje Ambiental: " << explorador->getPuntajeAmbiental() << endl;
+    cout << "Zona Actual: " << explorador->getZonaActual()->getNombre() << endl;
+    cout << "Elementos Disponibles: " << endl;
+    explorador -> getZonaActual() -> mostrarElementos();
 };
