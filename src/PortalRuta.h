c4771869b0e021ec65b0f4fe10ad2790e6161739
nataliaTm2 <ntorom22@gmail.com>
2026-05-26 09:47:49 -0500

    Update PortalRuta.h

--- src/PortalRuta.h
+++ src/PortalRuta.h
@@ -1,1 +1,16 @@
+#ifndef PORTALRUTA_H
+#define PORTALRUTA_H
+#include "ElementoInteractivo.h"
+#include "Zona.h"
 
+class PortalRuta : public ElementoInteractivo{
+    private:
+        Zona* destino;
+
+    public:
+        PortalRuta(Zona* destino);
+        void interactuar(Explorador* explorador) override;
+};
+
+
+#endif
