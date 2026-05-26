dcd6a8005340c0196a52114600b8a260821f03fc
nataliaTm2 <ntorom22@gmail.com>
2026-05-26 09:47:17 -0500

    Update PortalRuta.cpp

--- src/PortalRuta.cpp
+++ src/PortalRuta.cpp
@@ -1,1 +1,17 @@
+#include "PortalRuta.h"
+#include "ElementoInteractivo.h"
+#include "Explorador.h"
+
+#include <iostream>
+
+using namespace std;
+
+PortalRuta::PortalRuta(Zona* destino):ElementoInteractivo("Portal de Ruta"){
+    this ->destino = destino;
+}
+
+void PortalRuta::interactuar(Explorador* explorador){
+    cout << "Cambiaste de zona" << endl;
+    explorador -> cambiarZona(destino);
+}
 
