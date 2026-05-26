c453086d9c945df12c3c923bfaacc2f4aeeb78c6
nataliaTm2 <ntorom22@gmail.com>
2026-05-26 09:46:33 -0500

    Update PlantaMedicinal.cpp

--- src/PlantaMedicinal.cpp
+++ src/PlantaMedicinal.cpp
@@ -1,1 +1,19 @@
+#include "PlantaMedicinal.h"
+#include "AnimalHerido.h"
+#include "ElementoInteractivo.h"
+#include "Explorador.h"
+
+#include <iostream>
+
+using namespace std;
+
+PlantaMedicinal::PlantaMedicinal(): ElementoInteractivo("Planta Medicinal"){
+
+
+}
+
+void PlantaMedicinal::interactuar(Explorador* explorador){
+    cout << "Has recogido una planta medicinal." << endl;
+    explorador ->modificarEnergia(20);
+}
 
