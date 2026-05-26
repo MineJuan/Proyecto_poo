fbbeb3c2b923223c3c83e74506d7c48ad3cd70be
koielpez <soparedesr@gmail.com>
2026-05-26 09:39:30 -0500

    Update AnimalHerido.cpp

--- src/AnimalHerido.cpp
+++ src/AnimalHerido.cpp
@@ -1,1 +1,15 @@
+#ifndef ANIMALHERIDO_H
+#define ANIMALHERIDO_H
+
+#include "ElementoInteractivo.h"
+
+using namespace std;
+
+class AnimalHerido : public ElementoInteractivo {
+    
+public:
+    void interactuar(Explorador* explorador) override;
+};
+
+#endif
 
