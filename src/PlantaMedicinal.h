6a24b3bb221262179188d811b86ea106d77073c8
nataliaTm2 <ntorom22@gmail.com>
2026-05-26 09:46:55 -0500

    Update PlantaMedicinal.h

--- src/PlantaMedicinal.h
+++ src/PlantaMedicinal.h
@@ -1,1 +1,13 @@
+#ifndef PLANTAMEDICINAL_H
+#define PLANTAMEDICINAL_H
 
+#include "ElementoInteractivo.h"
+
+class PlantaMedicinal : public ElementoInteractivo{
+    public:
+        PlantaMedicinal();
+        void interactuar(Explorador* explorador)override;
+};
+
+
+#endif
