4054122bf61ddda34d8d61e70515be4ad1c2fab8
nataliaTm2 <ntorom22@gmail.com>
2026-05-26 09:49:40 -0500

    Update Zona.cpp

--- src/Zona.cpp
+++ src/Zona.cpp
@@ -1,5 +1,7 @@
+#include "Explorador.h"
 #include "zona.h"
 #include <iostream>
+
 
 using namespace std;
 
@@ -17,6 +19,18 @@ void Zona::agregarElemento(ElementoInteractivo* elemento){
 
 void Zona::mostrarElementos(){
     for (int i = 0; i < elementos.size(); i++){
-        cout << "Elemento " << i + 1 << "." << elementos[i]->getNombre() << endl;
+        cout << "Elemento " << i + 1 << ". " << elementos[i]->getNombre() << endl;
+    }
+}
+void Zona::interactuarConElemento(int indice, Explorador* explorador) {
+
+    elementos[indice]->interactuar(explorador);
+}
+void Zona::interactuarConElemento(string nombre, Explorador* explorador) {
+
+    for(int i = 0; i < elementos.size(); i++) {
+        if(elementos[i]->getNombre() == nombre) {
+            elementos[i]->interactuar(explorador);
+        }
     }
 }
