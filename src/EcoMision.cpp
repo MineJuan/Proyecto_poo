c7f7534d1aaa5c745553093951ce853449a0bc0f
MineJuan <juanvalvi4765@gmail.com>
2026-05-27 16:36:19 -0500

    ecomision  actualizado

--- src/EcoMision.cpp
+++ src/EcoMision.cpp
@@ -1,4 +1,7 @@
 #include "Ecomision.h"
+#include "AnimalHerido.h"
+#include "PlantaMedicinal.h"
+#include "PortalRuta.h"
 #include <iostream>
 
 using namespace std;
@@ -8,16 +11,57 @@ EcoMision::EcoMision() {
     explorador = new Explorador("Juan");
 }
 
-void EcoMision::iniciar(){
-    Zona* pantano = new Zona("Pantano");
-    explorador->cambiarZona(pantano);
-    cout << "Ecomision iniciada" << endl;
+void EcoMision::iniciar() {
 
+    //creamos zonas
+    Zona* pantano = new Zona("Pantano");
+    Zona* bosque = new Zona("Bosque");
+
+    //registramos las zonas en la reserva
+
+    reserva->agregarZona("pantano", pantano);
+    reserva->agregarZona("bosque", bosque);
+
+    //agregamos los elementos al pantano
+
+    pantano->agregarElemento(new AnimalHerido());
+
+    pantano->agregarElemento(new PlantaMedicinal());
+
+    pantano->agregarElemento(new PortalRuta(bosque));
+
+    //agregamos los elementos al bosque
+    bosque->agregarElemento(new PlantaMedicinal());
+
+    //ubicamos al explorador
+
+    explorador->cambiarZona(pantano);
+    
+    //iniciamos la mision
+    cout << "EcoMision iniciada: " << endl;
+
+    cout << endl;
+
+    //mostramos el estado inicial de todo
     mostrarRecorrido();
-};
+
+    cout << endl;
+
+    //aqui se realizan las interacciones
+    cout << "Interacciones:" << endl;
+
+    explorador->getZonaActual() ->interactuarConElemento(0, explorador); //interactuamos con el animal herido
+    explorador->getZonaActual() ->interactuarConElemento(1, explorador); //interactuamos con la planta medicinal
+    explorador->getZonaActual() ->interactuarConElemento(2, explorador); //interactuamos con el portal de ruta
+
+    cout << endl;
+
+    //mostramos estado final
+    mostrarRecorrido();
+}
 
 void EcoMision::mostrarRecorrido(){
-    cout << "-- Iniciar Recorrido --" << endl;
+    cout << "Recorrido hecho: " << endl;
     cout << "Explorador: " << explorador->getNombre() << endl;
     cout << "Energia: " << explorador->getEnergia() << endl;
     cout << "Puntaje Ambiental: " << explorador->getPuntajeAmbiental() << endl;
